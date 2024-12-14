c35959a31b07b54adf9ec5ab0653b15680a2dc37
eysteinndadi <150859182+eysteinndadi@users.noreply.github.com>
2024-12-14 17:10:25 +0000

    Beinagrind

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+import pygame
+from sys import exit
+
+pygame.init()
+
+
+class App():
+    def __init__(self) -> None:
+        #important variables
+        self.clock = pygame.time.Clock()
+
+        self.fps = 60
+        self.width = 1200
+        self.height = 800
+        self.wn = pygame.display.set_mode((self.width, self.height))
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+            
+            self.clock.tick(self.fps)
+            pygame.display.update()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
+
 
